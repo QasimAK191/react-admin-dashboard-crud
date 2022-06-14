3d07078d61bc9ddec9021bb6dd40729aa21a5bd0
Qasim Khan <69918464+QasimAK191@users.noreply.github.com>
2022-06-14 14:48:04 +0300

    Update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FiGithub } from 'react-icons/fi';
+
+const Footer = () => (
+    <div className="mt-24">
+        <hr />
+        <a href="www.github.com/QasimAK191">
+            
+        <p className="dark:text-gray-200 text-gray-700  m-20">
+                Follow My Github! <div style={{ flexDirection: 'row', justifyContent: 'flex-end' }}><FiGithub /></div> <i>Mohammed Qasim Khan</i>
+            </p>
+        </a>
+  </div>
+);
+
+export default Footer;
